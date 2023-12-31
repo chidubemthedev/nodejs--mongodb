1810b22b104ce988c61591e106788098c67c3339
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-12-31 18:37:37 +0100

    fixed delete issues

--- models/cart.js
+++ models/cart.js
@@ -43,7 +43,7 @@ module.exports = class Cart {
       if (err) {
         return;
       }
-      const updatedCart = { ...fileContent };
+      const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((prod) => prod.id === id);
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
