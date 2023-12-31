e318a192f1a37d69f8d2e9760991e47708df5ffb
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-12-31 09:49:26 +0100

    added cart

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,9 +1,14 @@
+const Cart = require("../models/cart");
+const Product = require("../models/product");
+
 exports.cartPage = (req, res, next) => {
   res.render("shop/cart", { pageTitle: "Your Cart", path: "/cart" });
 };
 
 exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId
-  console.log(prodId)
-  res.redirect("/cart")
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
 };
