27b6df4d1669ae9e994ffc6b2e42da863ef8fbf9
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-12-31 10:18:22 +0100

    added edit route

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
-const { getAddProductPage, postAddProduct, adminProductsPage } = require("../controllers/products");
+const {
+  getAddProductPage,
+  postAddProduct,
+  adminProductsPage,
+  getEditProduct,
+} = require("../controllers/products");
 
 const router = express.Router();
 
@@ -10,7 +15,8 @@ router.get("/add-product", getAddProductPage);
 // /admin/add-product => POST
 router.post("/add-product", postAddProduct);
 
-
 router.get("/products", adminProductsPage);
+
+router.get("/edit-product/:productId", getEditProduct);
 
 module.exports = router;
