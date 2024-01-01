06a034cee45e06339ec13359512a0f83ed65c1b6
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-01-01 08:36:16 +0100

    added cart delete item

--- controllers/cart.js
+++ controllers/cart.js
@@ -2,7 +2,32 @@ const Cart = require("../models/cart");
 const Product = require("../models/product");
 
 exports.cartPage = (req, res, next) => {
-  res.render("shop/cart", { pageTitle: "Your Cart", path: "/cart" });
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render("shop/cart", {
+        pageTitle: "Your Cart",
+        path: "/cart",
+        products: cartProducts,
+      });
+    });
+  });
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
 };
 
 exports.postCart = (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,26 @@
 const express = require("express");
 
-const { getProducts, productPage, index, getProduct  } = require("../controllers/products");
-const { cartPage, postCart } = require("../controllers/cart");
+const {
+  getProducts,
+  productPage,
+  index,
+  getProduct,
+} = require("../controllers/products");
+const {
+  cartPage,
+  postCart,
+  postCartDeleteProduct,
+} = require("../controllers/cart");
 const { checkout } = require("../controllers/checkout");
 
 const router = express.Router();
 
 router.get("/", index);
 router.get("/cart", cartPage);
-router.post("/cart", postCart)
+router.post("cart-delete-item", postCartDeleteProduct);
+router.post("/cart", postCart);
 router.get("/checkout", checkout);
-router.get('/products', getProducts);
-router.get('/products/:productId', getProduct)
-
+router.get("/products", getProducts);
+router.get("/products/:productId", getProduct);
 
 module.exports = router;
