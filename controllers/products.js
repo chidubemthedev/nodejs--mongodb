955d03d5db5ca25b27fef39dab8afab5daaf64d6
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-01-06 11:15:46 +0100

    alt using findall

--- controllers/products.js
+++ controllers/products.js
@@ -83,6 +83,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
+  // product.findAll({ where: { id: prodId } });
   Product.findByPk(prodId)
     .then((product) => {
       res.render("shop/product-details", {
