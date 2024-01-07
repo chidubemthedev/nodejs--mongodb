8e830b8ae4e01499bf27eabb159460c5b884b315
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-01-07 15:53:09 +0100

    added a user middleware

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -40,7 +49,7 @@ sequelize
     return user;
   })
   .then((user) => {
-    console.log(user);
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => console.log(err));
