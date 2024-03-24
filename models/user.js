cb7afb40cf8345062c6714d3b0524985cf21793c
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-03-24 09:13:05 +0100

    add to cart in user model

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const mongodb = require("mongodb");
 const { getDb } = require("../util/database");
 const { use } = require("../routes/admin");
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; //{items: []}
+    this._id = id;
   }
 
   save() {
@@ -16,6 +18,21 @@ class User {
         console.log("User Created!");
       })
       .catch((err) => console.log(err));
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id
+    // })
+
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
