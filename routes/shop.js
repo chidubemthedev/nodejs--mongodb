95a61ed797c079a98557e862b7eef0580a464381
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-03-24 12:35:07 +0100

    added get orders

--- routes/shop.js
+++ routes/shop.js
@@ -20,7 +20,7 @@ router.get("/cart", cartPage);
 router.post("/cart-delete-item", postCartDeleteProduct);
 router.post("/cart", postCart);
 // router.get("/checkout", checkout);
-// router.get("/orders", getOrders);
+router.get("/orders", getOrders);
 router.post("/create-order", postOrder);
 router.get("/products", getProducts);
 router.get("/products/:productId", getProduct);
