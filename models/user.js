3066b30fb9fe5cacbd64b6b8ccc74be98e10d642
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-03-26 10:56:08 +0100

    Refactor order retrieval in checkout controller and update order display in orders view

--- models/user.js
+++ models/user.js
@@ -70,32 +70,6 @@ userSchema.methods.clearCart = function () {
 
 module.exports = mongoose.model("User", userSchema);
 
-//   addOrder() {
-//     const db = getDb();
-//     return this.getCart()
-//       .then((products) => {
-//         const order = {
-//           items: products,
-//           user: {
-//             _id: new mongodb.ObjectId(this._id),
-//             name: this.name,
-//             email: this.email,
-//           },
-//         };
-
-//         return db.collection("orders").insertOne(order);
-//       })
-//       .then((result) => {
-//         this.cart = { items: [] };
-//         return db
-//           .collection("users")
-//           .updateOne(
-//             { _id: new mongodb.ObjectId(this._id) },
-//             { $set: { cart: { items: [] } } }
-//           );
-//       });
-//   }
-
 //   getOrders() {
 //     const db = getDb();
 //     return db
