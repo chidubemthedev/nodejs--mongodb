1cb21e9024e1494f5a9e7acaa8d959a5b0885347
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-03-29 14:30:54 +0100

    connect-mongodb-session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,16 @@
 exports.getLogin = (req, res, next) => {
-  isLoggedIn = req.get("Cookie").split(";")[2].trim().split("=")[1];
-  console.log(isLoggedIn);
+  // isLoggedIn = req.get("Cookie").split(";")[2].trim().split("=")[1];
+  // console.log(isLoggedIn);
 
+  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
-    isLoggedIn: isLoggedIn,
+    isLoggedIn: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
