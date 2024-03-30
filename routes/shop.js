8605afabeed9cc776c23158f97e44cd08a5f6454
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-03-30 12:17:44 +0100

    added protect routes on all relevant pages

--- routes/shop.js
+++ routes/shop.js
@@ -6,22 +6,26 @@ const {
   index,
   getProduct,
 } = require("../controllers/products");
+
 const {
   cartPage,
   postCart,
   postCartDeleteProduct,
 } = require("../controllers/cart");
+
 const { checkout, postOrder, getOrders } = require("../controllers/checkout");
+
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 router.get("/", index);
-router.get("/cart", cartPage);
-router.post("/cart-delete-item", postCartDeleteProduct);
-router.post("/cart", postCart);
+router.get("/cart", isAuth, cartPage);
+router.post("/cart-delete-item", isAuth, postCartDeleteProduct);
+router.post("/cart", isAuth, postCart);
 // router.get("/checkout", checkout);
-router.get("/orders", getOrders);
-router.post("/create-order", postOrder);
+router.get("/orders", isAuth, getOrders);
+router.post("/create-order", isAuth, postOrder);
 router.get("/products", getProducts);
 router.get("/products/:productId", getProduct);
 
